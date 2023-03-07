31878c7f6a01ae1e88826b9b6cc281cbdac420e4
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2023-03-07 14:50:40 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function profileAppear() {
+  document.getElementById("profile-shadow").style.display = "flex";
+}
+
+function profileDisappear() {
+  document.getElementById("profile-shadow").style.display = "none";
+}
